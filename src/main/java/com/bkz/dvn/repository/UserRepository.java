783f9689315a43b3e4de955388e3d687e3d7c56b
Dvwnmark <xcmark141@gmail.com>
2024-03-14 11:58:06 +0700

    fix prj

--- src/main/java/com/bkz/dvn/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bkz.dvn.repository;
-
-import com.bkz.dvn.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findFirstByUsername(String username);
-
-}
--- src/main/java/com/bkz/dvn/vo/SuccessVO.java
+++
@@ -1,9 +0,0 @@
-package com.bkz.dvn.vo;
-
-import lombok.Data;
-
-@Data
-public class SuccessVO {
-    private int code = 0;
-    private String message = "success";
-}
